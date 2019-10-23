6b2bea17ab999286b0efe7e63f6e11d6478c49ea
Shreyas85 <50212133+Shreyas85@users.noreply.github.com>
2019-10-23 10:12:04 +0530

    Uppdated with answer

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -2,9 +2,25 @@ package skeleton.answers;
 
 public class Question1 {
 
-    public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
-        // TODO Auto-generated method stub
-        return -5;
+    public static double calculateTotalPayment(double a, double b, double c) {
+        double tot;
+        int temp = (int) a;
+        double repay = (a*c)/100;
+        tot = (int) repay;
+        if(repay<50) repay = 50;
+        while(a>0){
+            double interest = (a*b)/100;
+            if(a<50){
+                repay = a;
+                a=0;
+            }
+            else{
+                a = a + interest;
+                a = a-repay;
+            }
+            tot = tot + (int) repay;
+        }
+        return tot;
     }
 
 }
