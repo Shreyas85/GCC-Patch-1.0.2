322355c4b9503568ddca998e7beac16553f34098
Shreyas85 <50212133+Shreyas85@users.noreply.github.com>
2019-10-23 10:16:35 +0530

    Answer

--- java/src/main/java/skeleton/answers/Question3.java
+++ java/src/main/java/skeleton/answers/Question3.java
@@ -2,8 +2,40 @@ package skeleton.answers;
 
 public class Question3 {
 
+    public static int printMode(int a[], int n) {
+       int max = Arrays.stream(a).max().getAsInt();
+        int t = max + 1;
+        int[] count = new int[t];
+        for (int i = 0; i < t; i++){
+            count[i] = 0;
+        }
+        for (int i = 0; i < n; i++){
+            count[a[i]]++;
+        }
+        int mode = 0;
+        int k = count[0];
+        for (int i = 1; i < t; i++){
+            if (count[i] >= k && i > mode){
+                k = count[i];
+                mode = i;
+            }
+        }
+        return mode;
+    }
+
     public static int whereDidIFinish(int[] scores, int[] alice) {
         // TODO Auto-generated method stub
-        return -5;
+        int n = scores.length;
+        int m = alice.length;
+        int count[];
+        count = new int[m];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(score[j] >=alice[i]){
+                    count[i]++;
+                }
+            }
+        }
+        printMode(count,m);
     }
 }
